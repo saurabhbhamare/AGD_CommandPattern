341663d1920b178b20909984f08efaee4266af6d
Saurabh bhamare <iamsaurabh@myyahoo.com>
2024-10-13 13:26:41 +0530

    Command Invoker Implementation

---
+++ Assets/Scripts/Command/CommandInvoker.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandInvoker
+{
+    public Stack<ICommand> commandRegistry = new Stack<ICommand>();
+    public void ProcessCommand(ICommand commandToProcess)
+    {
+        ExecuteCommand(commandToProcess);
+        RegisterCommand(commandToProcess);
+    }
+    public void ExecuteCommand(ICommand commandToExecute) => commandToExecute.Execute();
+
+    public void RegisterCommand(ICommand commandToRegister) => commandRegistry.Push(commandToRegister);
+}
+  
