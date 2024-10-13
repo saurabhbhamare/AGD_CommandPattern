a6479bbb75b78e1094945f4f50be9907801904fc
Saurabh bhamare <iamsaurabh@myyahoo.com>
2024-10-13 12:18:33 +0530

    Created  ICommand Interface and Abstract Command UnitCommand

---
+++ Assets/Scripts/Command/UnitCommand.cs
@@ -0,0 +1,15 @@
+
+public abstract class UnitCommand : ICommand
+{
+    public int ActorUnitID;
+    public int TargetUnitID;
+    public int ActorPlayerID;
+    public int TargetPlayerID;
+
+    protected UnitController actorUnit;
+    protected UnitController targetUnit;
+
+    public abstract void Execute();
+
+    public abstract bool WillHitTarget();
+}
